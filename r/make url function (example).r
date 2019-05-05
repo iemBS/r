5555926287cc6882cd3aecc449dc80365d0fc0d2
sburnell <sburnell@gmx.de>
2019-05-05 13:25:55 -0700

    Create make url function (example).r

---
+++ r/make url function (example).r
@@ -0,0 +1,5 @@
+url <- function(address, return.call = "json", sensor = "false") {
+  root <- "http://maps.google.com/maps/api/geocode/"
+  u <- paste(root, return.call, "?address=", address, "&sensor=", sensor, sep = "")
+  return(URLencode(u))
+}
